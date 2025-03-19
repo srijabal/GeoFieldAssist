6f743c5cc2b9189ad8fc044992d09b139e0535bf
Srija Bal <srijaabal@gmail.com>
2025-03-20 01:42:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:w-1/2 lg:mx-auto lg:gap-12 lg: gap-6">
         <Card className="hover:shadow-lg transition-shadow">
           <CardHeader>
             <CardTitle className="flex items-center">
